6563382b27cee597fde517931a9a6932a253819d
Lahbiyeb-Ismail <ismaillhbibe@gmail.com>
2024-04-15 18:37:23 +0100

    updating the Place, User and City models

--- models/city.py
+++ models/city.py
@@ -4,15 +4,17 @@ from models.base_model import BaseModel, Base
 from models import storage_type
 
 from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     __tablename__ = "cities"
 
-    if storage_type != "db":
-        name = ""
-        state_id = ""
-    else:
+    if storage_type == "db":
         name = Column(String(128), nullable=False)
         state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
+        places = relationship('Place', cascade="all,delete", backref="city")
+    else:
+        name = ""
+        state_id = ""
