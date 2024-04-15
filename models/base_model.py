2da1bd8273bc9e23c5bb8f7ee7453ce96a472674
Lahbiyeb-Ismail <ismaillhbibe@gmail.com>
2024-04-15 19:11:26 +0100

    updating the BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -7,13 +7,10 @@ import datetime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, String, DateTime
 
-
 if models.storage_type == "db":
     Base = declarative_base()
 else:
     Base = object
-
-time = "%Y-%m-%dT%H:%M:%S.%f"
 
 
 class BaseModel:
@@ -28,26 +25,19 @@ class BaseModel:
                             default=datetime.datetime.utcnow,
                             onupdate=datetime.datetime.utcnow)
 
-    def __init__(self, *args, **kwargs):
-        """Initialization of the base model"""
+    def __init__(self, *args, **kwargs) -> None:
+        """Initialization of BaseModel Class"""
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.datetime.now()
+        self.updated_at = datetime.datetime.now()
         if kwargs:
             for key, value in kwargs.items():
-                if key != "__class__":
+                if key in ["created_at", "updated_at"]:
+                    date = datetime.datetime.strptime(
+                        value, "%Y-%m-%dT%H:%M:%S.%f")
+                    setattr(self, key, date)
+                elif key != "__class__":
                     setattr(self, key, value)
-            if kwargs.get("created_at", None) and type(self.created_at) is str:
-                self.created_at = datetime.strptime(kwargs["created_at"], time)
-            else:
-                self.created_at = datetime.utcnow()
-            if kwargs.get("updated_at", None) and type(self.updated_at) is str:
-                self.updated_at = datetime.strptime(kwargs["updated_at"], time)
-            else:
-                self.updated_at = datetime.utcnow()
-            if kwargs.get("id", None) is None:
-                self.id = str(uuid.uuid4())
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.utcnow()
-            self.updated_at = self.created_at
 
     def __str__(self):
         """Returns a string representation of the instance"""
